029d2feb981cdc3908efae93c62220cbd762446d
Kevin Campusano <kcampusano@endpointdev.com>
2025-03-23 18:20:24 +0000

    Add Api client method to call GET product endpoint.

--- EndPointCommerce.WebStore/Api/ApiClient.cs
+++ EndPointCommerce.WebStore/Api/ApiClient.cs
@@ -3,17 +3,16 @@ namespace EndPointCommerce.WebStore.Api;
 public interface IApiClient
 {
     Task<List<Product>> GetProducts();
+    Task<Product> GetProduct(int id);
 }
 
 public class ApiClient : IApiClient, IDisposable
 {
     private readonly HttpClient _httpClient;
-    private readonly ILogger<ApiClient> _logger;
 
-    public ApiClient(HttpClient httpClient, ILogger<ApiClient> logger)
+    public ApiClient(HttpClient httpClient)
     {
         _httpClient = httpClient;
-        _logger = logger;
     }
 
     public void Dispose() => _httpClient?.Dispose();
@@ -23,10 +22,22 @@ public class ApiClient : IApiClient, IDisposable
         using var response = await _httpClient.GetAsync("api/Products");
         response.EnsureSuccessStatusCode();
 
-        _logger.LogInformation($"--> {response.RequestMessage?.Method} {response.RequestMessage?.RequestUri}");
-        _logger.LogInformation($"<-- {(int)response.StatusCode} {response.ReasonPhrase}");
-        _logger.LogDebug($"<-- {await response.Content.ReadAsStringAsync()}");
+        // Console.WriteLine($"--> {response.RequestMessage?.Method} {response.RequestMessage?.RequestUri}");
+        // Console.WriteLine($"<-- {(int)response.StatusCode} {response.ReasonPhrase}");
+        // Console.WriteLine($"<-- {await response.Content.ReadAsStringAsync()}");
 
         return (await response.Content.ReadFromJsonAsync<List<Product>>())!;
+    }
+
+    public async Task<Product> GetProduct(int id)
+    {
+        using var response = await _httpClient.GetAsync($"api/Products/{id}");
+        response.EnsureSuccessStatusCode();
+
+        // Console.WriteLine($"--> {response.RequestMessage?.Method} {response.RequestMessage?.RequestUri}");
+        // Console.WriteLine($"<-- {(int)response.StatusCode} {response.ReasonPhrase}");
+        // Console.WriteLine($"<-- {await response.Content.ReadAsStringAsync()}");
+
+        return (await response.Content.ReadFromJsonAsync<Product>())!;
     }
 }
