260a58b32ecc7072ba7a2cf9f84d7906a4884307
Kevin Campusano <kcampusano@endpointdev.com>
2025-03-23 18:22:02 +0000

    Remove unnecessary stuff from index page model.
    Related to Auth.NET and API URL configurations.

--- EndPointCommerce.WebStore/Pages/Index.cshtml.cs
+++ EndPointCommerce.WebStore/Pages/Index.cshtml.cs
@@ -6,26 +6,11 @@ namespace EndPointCommerce.WebStore.Pages;
 public class IndexModel : PageModel
 {
     private readonly IApiClient _apiClient;
-    private readonly string _authNetEnvironment = string.Empty;
 
-    public IndexModel(IApiClient apiClient, IConfiguration config)
+    public IndexModel(IApiClient apiClient)
     {
         _apiClient = apiClient;
-
-        _authNetEnvironment = config["AuthNetEnvironment"]!;
-
-        EndPointCommerceApiUrl = config["EndPointCommerceApiUrl"]!;
-        AuthNetLoginId = config["AuthNetLoginId"]!;
-        AuthNetClientKey = config["AuthNetClientKey"]!;
     }
-
-    public string EndPointCommerceApiUrl { get; set; }
-    public string AuthNetLoginId { get; set; }
-    public string AuthNetClientKey { get; set; }
-
-    public string AcceptJsUrl => _authNetEnvironment == "Production" ?
-        "https://js.authorize.net/v1/Accept.js" :
-        "https://jstest.authorize.net/v1/Accept.js";
 
     public async Task OnGetAsync()
     {
