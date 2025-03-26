15ce4efb13297e63e9b5d21249fbbd416e00191e
Kevin Campusano <kcampusano@endpointdev.com>
2025-03-26 18:09:47 +0000

    Further develop index page so that it fetches and renders products from the API.

--- EndPointCommerce.WebStore/Pages/Index.cshtml.cs
+++ EndPointCommerce.WebStore/Pages/Index.cshtml.cs
@@ -1,19 +1,29 @@
 using EndPointCommerce.WebStore.Api;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EndPointCommerce.WebStore.Pages;
 
-public class IndexModel : PageModel
+public class IndexModel : PageWithQuoteModel
 {
-    private readonly IApiClient _apiClient;
+    public IndexModel(IApiClient apiClient) : base(apiClient) { }
 
-    public IndexModel(IApiClient apiClient)
-    {
+    public List<Product> Products { get; set; } = [];
+
         _apiClient = apiClient;
     }
 
     public async Task OnGetAsync()
     {
+        Products = await _apiClient.GetProducts();
+    }
+
+    public async Task<IActionResult> OnPostAddToQuoteAsync(int productId)
+    {
+        var quoteItem = await _apiClient.PostQuoteItem(productId, 1, GetQuoteCookie());
+        if (quoteItem.Cookie != null) SetQuoteCookie(quoteItem.Cookie);
+
         var products = await _apiClient.GetProducts();
+
+        return RedirectToPage("/Index");
     }
 }
