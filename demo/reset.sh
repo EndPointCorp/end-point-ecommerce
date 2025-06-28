10746feda50247633d5fef66d25d0896a4fc84d1
Gered King <gered@endpointdev.com>
2025-06-28 16:47:54 -0400

    Add/update data and scripts used for the WebStore demo deployment
    Re-organized the files used for the WebStore demo, placing most anything
    used in that deployment under the top-level "demo" directory.
    
    Added sample product images, and updated the SQL data for the WebStore
    demo.
    
    Added reset.sh bash script used to completely reset the WebStore demo
    
    Updated Github Actions scripts for deploying the WebStore demo

---
+++ demo/reset.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+source .env
+
+# Ensure these are running (they might already be, but perhaps not if this is run as part of a deployment)
+docker compose up db maintenance -d --force-recreate
+
+# Stop the other containers that use the DB
+docker compose down admin-portal web-api web-store
+
+# Remove all existing images, and reset with the original reference demo store images
+rm -rf images/*
+cp -r reference-images/* images/
+
+# Remove everything from the DB and re-initialize from scratch
+docker compose exec db psql -U $END_POINT_COMMERCE_DB_USERNAME -c "drop owned by $END_POINT_COMMERCE_DB_USERNAME cascade;"
+docker compose exec maintenance run-migrations.sh
+
+# Add demo store data customizations
+docker compose cp demo/demo-data.sql db:/
+docker compose exec db psql -U $END_POINT_COMMERCE_DB_USERNAME -f /demo-data.sql
+
+# Add demo store admin user
+docker compose exec maintenance run-job.sh create_admin_user -u admin -e ecommerce-demo-admin@endpointdev.com -p Sh0pd3m0!
+
+# Bring up all the containers that we previously stopped
+docker compose up admin-portal web-api web-store -d --force-recreate
