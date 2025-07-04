ee218ba9506f805a3fbd489cad2b3fd40b98f5af
Kevin Campusano <kcampusano@endpointdev.com>
2025-07-04 16:37:00 -0400

    Adjust DI configs to add auth, api client and state container services.

--- EndPointCommerce.WebStoreSpa/Program.cs
+++ EndPointCommerce.WebStoreSpa/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using EndPointCommerce.WebStoreSpa;
 using EndPointCommerce.WebStoreSpa.Identity;
+using EndPointCommerce.WebStoreSpa.Api;
+using EndPointCommerce.WebStoreSpa.State;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,16 +13,20 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddAuthorizationCore();
 
 builder.Services.AddTransient<IncludeCredentialsHandler>();
-builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddHttpClient(
     "EndPointCommerce.WebApi",
     opt => opt.BaseAddress = new Uri(
-        builder.Configuration["WebApiUrl"] ??
-            throw new InvalidOperationException("Config setting 'WebApiUrl' not found.")
+        builder.Configuration["EndPointCommerceApiUrl"] ??
+            throw new InvalidOperationException("Config setting 'EndPointCommerceApiUrl' not found.")
     )
 ).AddHttpMessageHandler<IncludeCredentialsHandler>();
+
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<IApiClient, ApiClient>();
+builder.Services.AddSingleton<QuoteStateContainer>();
+builder.Services.AddSingleton<AlertStateContainer>();
 
 await builder.Build().RunAsync();
