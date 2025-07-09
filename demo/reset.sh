f13890788b4d769d831def86938f2433f92e1da5
Gered King <gered@endpointdev.com>
2025-07-09 16:09:15 -0400

    Simplify reset.sh

--- demo/reset.sh
+++ demo/reset.sh
@@ -3,18 +3,11 @@ cd "$(dirname "$0")"
 
 source .env
 
-# Ensure these are running (they might already be, but perhaps not if this is run as part of a deployment)
-docker compose up db maintenance -d --force-recreate
+# Ensure everything is stopped, and fully remove all persistent volumes as well to blow away the DB
+docker compose down -v
 
-# Stop the other containers that use the DB
-docker compose down admin-portal web-api web-store
-
-# Remove all existing images, and reset with the original reference demo store images
-rm -rf images/*
-cp -r reference-images/* images/
-
-# Remove everything from the DB and re-initialize from scratch
-docker compose exec db psql -U $END_POINT_COMMERCE_DB_USERNAME -c "drop owned by $END_POINT_COMMERCE_DB_USERNAME cascade;"
+# Bring up the database and run the migrations
+docker compose up db maintenance -d
 docker compose exec maintenance run-migrations.sh
 
 # Add demo store data customizations
@@ -24,5 +17,9 @@ docker compose exec db psql -U $END_POINT_COMMERCE_DB_USERNAME -f /demo-data.sql
 # Add demo store admin user
 docker compose exec maintenance run-job.sh create_admin_user -u admin -e ecommerce-demo-admin@endpointdev.com -p Sh0pd3m0!
 
-# Bring up all the containers that we previously stopped
-docker compose up admin-portal web-api web-store -d --force-recreate
+# Remove all existing images, and reset with the original reference demo store images
+rm -rf images/*
+cp -r reference-images/* images/
+
+# Now we can bring up all the containers
+docker compose up -d
