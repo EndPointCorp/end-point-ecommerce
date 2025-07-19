4d0ba057052084f63b588204df5028ca016ffd92
Kevin Campusano <kcampusano@endpointdev.com>
2025-07-19 14:18:16 -0400

    Move duplicated logic for formatting dates and currencies into a reusable extension methods.

---
+++ EndPointCommerce.WebStoreSpa/Formatters/Formatters.cs
@@ -0,0 +1,12 @@
+using System.Globalization;
+
+namespace EndPointCommerce.WebStoreSpa.Formatters;
+
+public static class Formatters
+{
+    public static string AsLongDate(this DateTime value) =>
+        value.ToString("D", CultureInfo.GetCultureInfo("en-US"));
+
+    public static string AsCurrency(this decimal value) =>
+        value.ToString("C", CultureInfo.GetCultureInfo("en-US"));
+}
