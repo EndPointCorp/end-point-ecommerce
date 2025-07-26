dc34c744e696020958e439e37be35fab7b1d5bf8
Kevin Campusano <kcampusano@endpointdev.com>
2025-07-25 20:09:08 -0400

    Define the email templates as .razor files instead of .cshtml files.
    More importantly, use the razor components rendering services instead of the razor pages one. This makes the email rendering logic much simpler.

--- EndPointCommerce.RazorTemplates/Templates.cs
+++
@@ -1,8 +0,0 @@
-namespace EndPointCommerce.RazorTemplates;
-
-public static class Templates
-{
-    public static string AccountConfirmation => "/Views/Emails/AccountConfirmation.cshtml";
-    public static string PasswordReset => "/Views/Emails/PasswordReset.cshtml";
-    public static string OrderConfirmation => "/Views/Emails/OrderConfirmation.cshtml";
-}
