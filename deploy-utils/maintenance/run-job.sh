d9e7cd6566e048871454ee81ed0f04e9696309a0
Gered King <gered@endpointdev.com>
2025-08-13 14:36:51 -0400

    Include current directory sanity check in run-job.sh

--- deploy-utils/maintenance/run-job.sh
+++ deploy-utils/maintenance/run-job.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+if [ ! -d "./EndPointEcommerce.Infrastructure" ]; then
+  echo "Run this from the same directory as end-point-ecommerce.sln."
+  exit 1
+fi
+
 export ConnectionStrings__EndPointEcommerceDbContext=$(cat /run/secrets/end-point-ecommerce-db-connection-string)
 dotnet run --project ./EndPointEcommerce.Jobs -- $@
